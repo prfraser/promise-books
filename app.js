b9b44c85cf7bc51ec1c81dfa704395c738cc27a7
philip fraser <pr.fraser3@gmail.com>
2017-12-04 13:47:39 +1000

    async/await CRU

--- app.js
+++ app.js
@@ -73,22 +73,58 @@ let Book = require('./models/book');
 //    .then(res => console.log(res))
 //    .catch(err => console.log(err))
 
-Promise.all([
-		Book.create({ 
-		name: 'Cool Book', 
-		author: 'George RR Martin', 
-		description: 'cool book' }),
-		Book.find(),
-		Book.findOneAndUpdate(
-		   { "name" : "Cool Book" },
-		   { "name" : "Really Cool Book" }),
-		Book.findOneAndRemove(
-	   { "name" : "Really Cool Book" }),
-		Book.find()
-	]).then(([res1, res2, res3, res4, res5]) => {
-		console.log(res1)
-		console.log(res2)
-		console.log(res3)
-		console.log(res4)
-		console.log(res5)
-	})
+// Promise.all([
+// 		Book.create({ 
+// 		name: 'Cool Book', 
+// 		author: 'George RR Martin', 
+// 		description: 'cool book' }),
+// 		Book.find(),
+// 		Book.findOneAndUpdate(
+// 		   { "name" : "Cool Book" },
+// 		   { "name" : "Really Cool Book" }),
+// 		Book.findOneAndRemove(
+// 	   { "name" : "Really Cool Book" }),
+// 		Book.find()
+// 	]).then(([res1, res2, res3, res4, res5]) => {
+// 		console.log(res1)
+// 		console.log(res2)
+// 		console.log(res3)
+// 		console.log(res4)
+// 		console.log(res5)
+// 	})
+
+// async function createBook() {
+// 	try {
+// 	    const newbook = await Book.create({ 
+// 				name: 'Cool Book', 
+// 				author: 'George RR Martin', 
+// 				description: 'cool book' })
+// 	    console.log(newbook);
+// 	    return newbook;
+// 	  } catch (err) {
+// 	    return 'error occured';
+// 	  }
+// }
+// createBook()
+
+// async function findBooks() {
+// 	try {
+// 		const foundBooks = await Book.find()
+// 		console.log(foundBooks)
+// 	} catch (err) {
+// 		console.log(err)
+// 	}
+// }
+// findBooks()
+
+// async function updateBook() {
+// 	try {
+// 		const updatedBook = await Book.findOneAndUpdate(
+// 	   { "name" : "Cool Book" },
+// 	   { "name" : "Random book name" })
+// 		console.log(updatedBook)
+// 	} catch (err) {
+// 		console.log(err)
+// 	}
+// }
+// updateBook()
