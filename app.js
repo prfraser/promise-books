5c1288a2cdaa8d88b06d6d4792ced2439fa41300
philip fraser <pr.fraser3@gmail.com>
2017-12-04 12:21:25 +1000

    add, find, update with callbacks

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 // const express = require('express');
 let Book = require('./models/book');
 
+// Book.create({ 
+// 	name: 'A Game of Thrones', 
+// 	author: 'George RR Martin', 
+// 	description: 'cool book' }, (error, response) => {
+//   console.log(response)
+
+// });
+
+// Book.find((err, response) => {
+// 	console.log(response)
+// })
+
+// Book.findOneAndUpdate(
+//    { "name" : "A Game of Thrones" },
+//    { "name" : "A Song of Ice and Fire" }, (err, response) => {
+// 	console.log(response)
+// })
